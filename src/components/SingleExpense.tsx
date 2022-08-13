80950eecade72009db5c20ff1a800c0c9d6eb530
emre <emreozturanli1@gmail.com>
2022-08-13 10:18:34 +0300

    remove expense is done

--- src/components/SingleExpense.tsx
+++ src/components/SingleExpense.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react"
+import { FC, useContext } from "react"
 import { CloseButton, ListGroup } from "react-bootstrap"
+import { ExpenseContext } from "../context/expenseContext"
 import { IExpenses } from "../types/interfaces"
 
 interface ISingleExpensePorps {
@@ -8,11 +9,20 @@ interface ISingleExpensePorps {
 
 const SingleExpense:FC<ISingleExpensePorps> = (props) => {
   const {expense} = props
+  const {dispatch} = useContext(ExpenseContext)
+
+  const handleRemove = () =>{
+    dispatch({type:'REMOVE_EXPENSE', payload:expense.id})
+  }
+
   return (
     <ListGroup.Item className="d-flex align-items-center">
         <h5>{expense.name}</h5>
         <p className="ms-auto me-4 mb-0">${expense.cost}</p>
-       <CloseButton style={{backgroundColor:'#a0f9f9'}}/>
+       <CloseButton 
+       style={{backgroundColor:'#a0f9f9'}}
+       onClick={handleRemove}
+       />
       </ListGroup.Item>
   )
 }
